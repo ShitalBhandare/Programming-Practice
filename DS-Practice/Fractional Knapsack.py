4a626d8004ba55cde1812b5e92b2eb485cbf87b7
ShitalBhandare <bhandareshital19@gmail.com>
2019-08-31 22:34:58 +0530

    Fractional Knapsack Problem

---
+++ DS-Practice/Fractional Knapsack.py
@@ -0,0 +1,50 @@
+# Hello World program in Python
+    
+#python 3.5.2
+
+# Fractional Knapsack Problem
+
+
+def knapsack(W, wt, val, n):
+    
+    ratio = []
+    for i in range(n):
+        ratio.append([val[i]/wt[i], wt[i], val[i]])
+    
+    ratio.sort(reverse= True)
+    print(ratio)
+    totalvalue = 0
+    for item in ratio:
+        cur_wt = item[1]
+        cur_val = item[2]
+        
+        if W - cur_wt >= 0:
+            W -= cur_wt
+            totalvalue += cur_val
+        else:
+            print("here")
+            fraction = W // cur_wt
+            print(fraction)
+            totalvalue += fraction * cur_val
+            print(totalvalue)
+            W -= cur_wt * fraction
+            break
+        
+    return totalvalue
+    
+
+if __name__ == "__main__":
+    
+    #wt = [10, 20, 30]
+    #val = [60, 100, 120]
+    #W = 50
+    
+    wt = [10, 40, 20, 30] 
+    val = [60, 40, 100, 120] 
+    W = 50
+    
+    max_val = knapsack(W, wt, val, len(wt))
+    print("Maximum value achieved:", max_val)
+    
+    
+

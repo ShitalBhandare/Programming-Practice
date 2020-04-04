be6d6c04e8ddd9456be10502bdde8a95d8490e67
Shital Bhandare <shital.bhandare@druva.local>
2020-04-04 14:35:07 +0530

    Intro to Short Declaration in GO

---
+++ GoLang-Practice/003_intro_to_short_declaration.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 99
+	fmt.Println(x)
+	x = 100
+	fmt.Println(x)
+	y := 19 + 1
+	fmt.Println(y)
+	z := "Intro to Short Declaration in Go"
+	fmt.Println(z)
+}

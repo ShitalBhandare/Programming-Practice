7b08a6ddbbf07cd950af4aba70660888d84175d1
Shital Bhandare <shital.bhandare@druva.local>
2020-04-15 22:22:46 +0530

    Intro to INT data type

---
+++ GoLang-Practice/014_intro_to_int_data_type.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var a int = 12
+var b float64 = 15
+
+func main() {
+
+	x := 42.1211
+	y := 20
+	fmt.Println("x:",x)
+	fmt.Println("y:",y)
+
+	fmt.Printf("%T\n",x)
+	fmt.Printf("%T\n",y)
+
+	x = 175  // This will work
+	fmt.Println("x:",x)
+
+	// y = 35.13 This will not work
+
+	fmt.Println("a:",a)
+	fmt.Println("b:",b)
+
+	fmt.Println("GOOS:",runtime.GOOS)
+	fmt.Println("GOARCH:",runtime.GOARCH)
+}
+

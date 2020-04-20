d4bc63b3479becc52273cd634bdf4c42f770e5f9
Shital Bhandare <shital.bhandare@druva.local>
2020-04-20 16:52:02 +0530

    Intro to numeral system

---
+++ GoLang-Practice/016_intro_to_numeral_system.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s = "S"
+	fmt.Println(s)
+	fmt.Printf("%s\n", s)
+
+	bs := []byte(s)
+	n := bs[0]
+
+	fmt.Printf("Decimal:%d\n", n)
+	fmt.Printf("Hex:%x\n", n)
+	fmt.Printf("Binary:%b\n", n)
+	fmt.Printf("Octal:%o\n", n)
+
+	// %#x => Prepends 0x
+	fmt.Printf("Decimal:%#x\n", n)
+}
+

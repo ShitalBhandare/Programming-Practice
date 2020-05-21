eb560afb253b6776b44f7328c4df4fc23e431714
Shital Bhandare <shital.bhandare@druva.local>
2020-05-21 18:20:08 +0530

    Intro to structs

---
+++ GoLang-Practice/030_intro_to_struct.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+// STRUCT: Composite Data Type which holds values of different type
+// TYPE person with underlined data type struct
+
+type person struct{
+	first string
+	last string
+	age int
+}
+
+// Embedded struct
+
+type employee struct{
+	person
+	emp_id int
+}
+
+func main() {
+
+	p1 := person{
+		first: "Shital",
+		last: "Bhandare",
+		age: 24,
+	}
+	fmt.Println(p1)
+	fmt.Println(p1.first, p1.last, p1.age)
+
+	e1 := employee{
+		person : person{
+			first: "ABC",
+			last: "XYZ",
+			age: 32,
+		},
+		emp_id: 1873,
+	}
+
+	fmt.Println(e1)
+	fmt.Println(e1.person, e1.emp_id)
+
+	// To resolve the name collision
+	fmt.Println(e1.person.first, e1.person.last, e1.person.age, e1.emp_id)
+
+	// Anonymous struct
+	// Can be used to avoid code pollution
+
+	person1 := struct {
+		first string
+		last  string
+		age   int
+	}{
+		first: "Shital",
+		last:  "Bhandare",
+		age:   24,
+	}
+	fmt.Println(person1)
+}
+

fc42d627573ae677eb66395359c348a2215cdab4
Shital Bhandare <shital.bhandare@druva.local>
2020-05-23 18:05:21 +0530

    Corrected code for function with variadic parameters

--- GoLang-Practice/033_intro_to_variadic_parameters_for_func.go
+++ GoLang-Practice/033_intro_to_variadic_parameters_for_func.go
@@ -6,8 +6,8 @@ import (
 
 
 func main() {
-	sum := sum(1, 2, 3, 4, 5)
-	fmt.Println("The sum is", sum)
+	sum1 := sum(1, 2, 3, 4, 5)
+	fmt.Println("The sum is", sum1)
 
 	lp := sum(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
 	fmt.Println("The sum is", lp)

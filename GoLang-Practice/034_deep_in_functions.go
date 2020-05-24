c83b9ed7fb6319514e31752cbf4123b5dffa1fef
Shital Bhandare <shital.bhandare@druva.local>
2020-05-24 14:59:03 +0530

    Unfurling a slice

---
+++ GoLang-Practice/034_deep_in_functions.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	s1 := []int{1, 2, 3, 4, 5}
+	sum1 := sum(s1...)		// Unfurling a slice
+	fmt.Println("The sum is", sum1)
+
+	// sum1 := sum(s1)		// This will not work since function sum is expecting int values and not a slice of int
+
+	sum1 = sum()			// It works since variadic parameters means ZERO or MORE values
+	fmt.Println("The sum is", sum1)
+
+	//sum1 = sum(s1..., "James")	// This will not work. Since VARIADIC Parameter should always be a FINAL parameter to function.
+	//sum1 = sum("James", s1...)	// This will work.
+
+}
+
+func sum(x ...int) int {		// Function with variadic parameters.
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)		// Slice of int
+
+	sum := 0
+	for _, v := range x {
+		sum += v
+	}
+	return sum
+}
+

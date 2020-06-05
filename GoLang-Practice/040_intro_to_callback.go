c876b22ac398ae4cb13734bedd84e738a6d58942
Shital Bhandare <shital.bhandare@druva.local>
2020-06-05 17:48:57 +0530

    Intro to callback

---
+++ GoLang-Practice/040_intro_to_callback.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	s1 := sum(slice...)
+	fmt.Println("Total Sum:", s1)
+
+	// Callback is passing a func as an argument to other func
+	s2 := evenSum(sum, slice...)
+	fmt.Println("Even Sum:", s2)
+
+	s3 := oddSum(sum, slice...)
+	fmt.Println("Odd Sum:", s3)
+}
+
+func sum(y ...int) int {
+	total := 0
+	for _, v := range y {
+		total += v
+	}
+	return total
+}
+
+func evenSum(f func(l ...int) int, j ...int) int {
+	var xi []int
+	for _, v := range j {
+		if v % 2 == 0 {
+			xi = append(xi, v)
+		}
+	}
+
+	return f(xi...)
+}
+
+func oddSum(f func(l ...int) int, j ...int) int {
+	var xi []int
+	for _, v := range j {
+		if v % 2 != 0 {
+			xi = append(xi, v)
+		}
+	}
+
+	return f(xi...)
+}
+

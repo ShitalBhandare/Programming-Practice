f23802121e17824ff2cb653ed73f544fb7c9a9f4
Shital Bhandare <shital.bhandare@druva.local>
2020-06-09 16:56:22 +0530

    Level 6 Exercise 1 to 5

---
+++ GoLang-Practice/043_level_6_exercise_1_to_5.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+type square struct {
+	length float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type shape interface {
+	area() float64
+}
+
+func main() {
+
+	// Exercise 1
+	fmt.Println(foo())
+	fmt.Println(bar())
+
+	// Exercise 2
+	s1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	// Passing int values.. i.e. unfurling the slice
+	fmt.Println(foo1(s1...))
+
+	// Passing slice of int
+	fmt.Println(bar1(s1))
+
+	// Exercise 3
+	defer func_to_defer()
+	demo()
+	test()
+
+	// Exercise 4
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+		age:   32,
+	}
+
+	p1.speak()
+
+	// Exercise 5
+	s2 := square{
+		length: 10.76,
+	}
+
+	c1 := circle{
+		radius: 5.082,
+	}
+	fmt.Println(s2)
+	fmt.Println(c1)
+	info(s2)
+	info(c1)
+}
+
+func foo() int {
+	return 19
+}
+
+func bar() (int, string) {
+	return 1873, "Shital"
+}
+
+func foo1(xi ...int) int {
+	total := 0
+	for _, v := range xi {
+		total += v
+	}
+	return total
+}
+
+func bar1(xi []int)  int{
+	total := 0
+	for _, v := range xi {
+		total += v
+	}
+	return total
+}
+
+func func_to_defer() {
+	fmt.Println("Func to defer")
+}
+
+func demo() {
+	fmt.Println("Demo func")
+}
+
+func test() {
+	fmt.Println("test func")
+}
+
+func (p person) speak() {
+	fmt.Println("I am", p.first, p.last, "and I am", p.age, "years old.")
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (s square) area() float64 {
+	return s.length * s.length
+}
+
+func info(s shape) {
+	fmt.Println(s.area())
+}

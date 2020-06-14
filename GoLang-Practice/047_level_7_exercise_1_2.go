72fed147cd6fccfbc76c20fb20e4a2c826d27a07
Shital Bhandare <shital.bhandare@druva.local>
2020-06-14 20:02:43 +0530

    Level 7 Exercise 1 & 2

---
+++ GoLang-Practice/047_level_7_exercise_1_2.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type person struct{
+	first string
+	last string
+	age int
+}
+
+func changeMe(p *person){
+	//(*p).first = "James"
+	p.first = "James"		// This works
+	(*p).last = "Bond"
+	(*p).age = 34
+}
+
+
+func main() {
+
+	// Exercise 1
+	x := 20
+	fmt.Println("x:\t", x)
+	fmt.Println("&x:\t", &x)
+
+	// Exercise 2
+	p := person{
+		first: "Shital",
+		last: "Bhandare",
+		age: 24,
+	}
+	fmt.Println(p)
+	changeMe(&p)
+	fmt.Println(p)
+}

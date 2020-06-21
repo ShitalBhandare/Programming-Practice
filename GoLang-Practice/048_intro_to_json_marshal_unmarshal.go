eb9f004ed132704d31c4835f2f4699adafbc3cb3
Shital Bhandare <shital.bhandare@druva.local>
2020-06-21 11:33:05 +0530

    Intro to JSON Marshal and Unmarshal

---
+++ GoLang-Practice/048_intro_to_json_marshal_unmarshal.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string				// If identifiers starts with small letter, it will not be exported in JSON encoding.
+	Last  string
+	Age   int
+}
+
+type employee struct {
+	First string `json:"First"`		// Tag: `json:"First"` => customized json
+	Last  string `json:"Last"`		// It specifies that json key "First" will point to Field "First" in Go
+	Age   int    `json:"Age"`
+}
+
+func main() {
+
+	p1 := person{
+		First: "Shital",
+		Last:  "Bhandare",
+		Age:   24,
+	}
+
+	p2 := person{
+		First: "Miss",
+		Last:  "Monneypenny",
+		Age:   32,
+	}
+	fmt.Println("\nBefore Marshalling")
+	fmt.Println(p1, p2)
+
+
+	// Marshal: Json encoding
+
+	people := []person{p1, p2}
+	bs, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println("Failed to marshal, ", err)
+	}
+	fmt.Println("\nAfter Marshalling")
+	fmt.Println(string(bs))
+
+
+
+	// Unmarshal: JSON Decoding to Go Type
+
+	var employees []employee
+	err = json.Unmarshal(bs, &employees)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("\nAfter Unmarshalling")
+	fmt.Println(employees)
+
+}
+

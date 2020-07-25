b6ff4ac3844306c7da543436d0e87781e829ad64
Shital Bhandare <shital.bhandare@druva.local>
2020-07-25 16:34:56 +0530

    Wrapping goroutine value around channel

---
+++ GoLang-Practice/059_wrapping_goroutine_value_around_channel.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func doSomething(i int) int {
+	return i*5
+}
+
+func main() {
+
+	ch := make(chan int)
+
+	// New go routine throws out the value returned.
+	// Hence, wrap around the value in channel
+	go func() {
+		ch <- doSomething(3)
+	}()
+	fmt.Println(<-ch)
+}
+

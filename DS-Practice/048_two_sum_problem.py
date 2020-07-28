568f6c9bc955a0490a871be27b62d67700d56ca9
Shital Bhandare <shital.bhandare@druva.local>
2020-07-28 22:05:34 +0530

    Two Sum Problem

---
+++ DS-Practice/048_two_sum_problem.py
@@ -0,0 +1,30 @@
+'''
+Python TwoSum Problem:
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        map1 = {}
+        for i, val in enumerate(nums):
+            diff = target - val
+            if diff in map1:
+                return map1[diff], i
+            else:
+                map1[val] = i

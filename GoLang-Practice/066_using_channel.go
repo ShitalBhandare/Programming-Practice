fa79a2d3ab5f0312a1088437064a21e0f29ede9c
Shital Bhandare <shital.bhandare@druva.local>
2020-08-25 09:40:53 +0530

    Using channels

---
+++ GoLang-Practice/066_using_channel.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	c := make(chan int)
+
+	// send 
+	go foo(c)
+
+	// receive
+	//go bar(c)			// these go routines will never send/receive at the same time
+	bar(c)
+
+	fmt.Println("About to exit")
+}
+
+// send
+func foo(c chan <- int){
+	c <- 19
+}
+
+// receive
+func bar(c <- chan int){
+	fmt.Println(<-c)
+}
+
+

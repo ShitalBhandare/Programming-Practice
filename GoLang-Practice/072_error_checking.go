f83d727bad59d16d4ef9fcc4f969f4ff48516a06
Shital Bhandare <shital.bhandare@druva.local>
2020-09-13 15:11:31 +0530

    Error checking

---
+++ GoLang-Practice/072_error_checking.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Always check the errors
+	// Unlike Python, Go does not have try, catch finally statements to catch exceptions.
+
+	n, err := fmt.Println("Shital")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("No of bytes written:", n)
+
+	var ans1, ans2, ans3 string
+
+	fmt.Print("Fav Food:")
+	_, err = fmt.Scan(&ans1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Print("Fav Sport:")
+	_, err = fmt.Scan(&ans2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Print("Fav Drink:")
+	_, err = fmt.Scan(&ans3)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(ans1, ans2, ans3)
+}
+

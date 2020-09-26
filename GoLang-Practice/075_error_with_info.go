7ac3cd50bf5fbbe21b9abbc5d630615663e416fb
shital.bhandare <shital.bhandare@druva.com>
2020-09-26 16:59:23 +0530

    Errors with Info

---
+++ GoLang-Practice/075_error_with_info.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+
+	// Errors with more info
+
+	v, err := sqrt(-10)
+	fmt.Println(v, err)
+
+	v, err = cuberrot(-5)
+	fmt.Println(v, err)
+}
+
+func sqrt(f float64) (float64, error) {
+	fmt.Println("In sqrt func")
+	if f < 0 {
+		// Plain error string
+		er := errors.New("Invalid value to get square root")
+		return 0.0, er
+	}
+	return 41, nil
+}
+
+func cuberrot(f float64) (float64, error) {
+	fmt.Println("In cuberoot func")
+	if f < 0 {
+		// Formatted error string
+		er := fmt.Errorf("Invalid value %f to get cube root", f)
+		return 0.0, er
+	}
+	return 42, nil
+}
+

ef1a675917953851d00bd1bcc99aa9288f7661b8
shital.bhandare <shital.bhandare@druva.com>
2020-12-15 12:20:10 +0530

    Cycle in Linked list par 2

---
+++ DS-Practice/LeetCode_Problems/024_cycle_in_linked_list_part2.py
@@ -0,0 +1,37 @@
+'''
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Notice that you should not modify the linked list.
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow_ptr = fast_ptr = ptr = head
+        
+        while fast_ptr and fast_ptr.next:
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+            
+            if slow_ptr == fast_ptr:
+                # Cycle Present
+                while slow_ptr != ptr:
+                    slow_ptr = slow_ptr.next
+                    ptr = ptr.next
+                
+                return ptr
+                
+        return None
+
+'''
+Time Complexity: O(n
+Space Complexity: O(1)
+'''

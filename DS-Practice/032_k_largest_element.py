f87c494988f4d13b82b791c84c3e0965a52684f9
ShitalBhandare <bhandareshital19@gmail.com>
2025-03-21 14:50:23 +0530

    Update 032_k_largest_element.py
    K Largest Elements using python heapq module.

--- DS-Practice/032_k_largest_element.py
+++ DS-Practice/032_k_largest_element.py
@@ -69,3 +69,48 @@ if __name__ == "__main__":
     print('K largest elements are as follows:')
     for i in range(k):
         print(arr[i], end=' ')
+
+
+========================================
+
+
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+# Python program to find the k largest elements in the  
+# array using min heap
+
+import heapq
+
+# Function to find the k largest elements in the array 
+def kLargest(arr, k):
+  
+    # Create a min-heap with the first k elements
+    minH = arr[:k]
+    heapq.heapify(minH)
+    print(minH)
+
+    
+    # Traverse the rest of the array
+    for x in arr[k:]:
+        if x > minH[0]:
+            heapq.heapreplace(minH, x)
+            print(minH)
+    
+    res = []
+
+    # Min heap will contain only k 
+    # largest element
+    while minH:
+        res.append(heapq.heappop(minH))
+
+    # Reverse the result array, so that all
+    # elements are in decreasing order
+    res.reverse()
+
+    return res
+
+if __name__ == "__main__":
+    arr = [1, 23, 12, 9, 30, 2, 50]
+    k = 3
+    res = kLargest(arr, k)
+    print(" ".join(map(str, res)))

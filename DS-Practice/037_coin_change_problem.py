16b33fe32a2323aa7375e87a988252cc35446e5c
ShitalBhandare <bhandareshital19@gmail.com>
2025-04-06 23:27:00 +0530

    Update 037_coin_change_problem.py

--- DS-Practice/037_coin_change_problem.py
+++ DS-Practice/037_coin_change_problem.py
@@ -32,3 +32,53 @@ if __name__ == "__main__":
     
     result = find_coin_change(coins, n, sum)
     print("No. of ways we achieve the sum using coins: ", result)
+
+
+=============================
+'''
+We have infinite supply of coins.
+coins = [1, 2, 3]
+sum = 4
+We need to find minimum number of coins using which we can achieve the given sum.
+Minimum no of coins = 2
+[3, 1]
+'''
+
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+def find_coin_change(coins, n, sum):
+    
+    matrix = [[0 for j in range(sum+1)] for i in range(n+1)]
+
+    for j in range(sum+1):
+        matrix[0][j] = j
+        
+    print(matrix)
+        
+    for i in range(1, n+1):
+        for j in range(1, sum+1):
+            
+            if j >=coins[i-1]:
+                matrix[i][j] = min(1 + matrix[i][j-coins[i-1]], matrix[i-1][j])
+                print(matrix)
+
+            else:
+                print("here")
+                matrix[i][j] = matrix[i-1][j]
+                print(matrix)
+                
+    print(matrix)
+
+                
+    return matrix[n][sum]
+
+if __name__ == "__main__":
+    coins = [1, 5, 6, 8]
+    sum = 11
+    n = len(coins)
+    
+    result = find_coin_change(coins, n, sum)
+    print("No. of ways we achieve the sum using coins: ", result)
+
+
+

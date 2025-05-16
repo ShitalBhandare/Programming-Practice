e7af0727e7cdfd0b1bd7cd1a98597edde5b4364e
ShitalBhandare <bhandareshital19@gmail.com>
2025-05-16 20:01:44 +0530

    Update 068_channel_select_clause.go

--- GoLang-Practice/068_channel_select_clause.go
+++ GoLang-Practice/068_channel_select_clause.go
@@ -44,3 +44,31 @@ func receive(eve, odd, quit <-chan int) {
 	}
 }
 
+=====================OUTPUT================
+
+
+From eve channel: 0
+From odd channel: 1
+From eve channel: 2
+From odd channel: 3
+From eve channel: 4
+From odd channel: 5
+From eve channel: 6
+From odd channel: 7
+From eve channel: 8
+From odd channel: 9
+From eve channel: 10
+From odd channel: 11
+From eve channel: 12
+From odd channel: 13
+From eve channel: 14
+From odd channel: 15
+From eve channel: 16
+From odd channel: 17
+From eve channel: 18
+From odd channel: 19
+From quit channel: 0
+About to exit
+
+Program exited.
+

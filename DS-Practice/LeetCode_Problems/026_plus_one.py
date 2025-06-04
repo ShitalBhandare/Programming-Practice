dcb45645c2b59974d09b7460aea9050c83eeae1d
ShitalBhandare <bhandareshital19@gmail.com>
2025-06-04 20:21:05 +0530

    Update 026_plus_one.py

--- DS-Practice/LeetCode_Problems/026_plus_one.py
+++ DS-Practice/LeetCode_Problems/026_plus_one.py
@@ -4,6 +4,14 @@ Given a non-empty array of decimal digits representing a non-negative integer, i
 The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
 
 You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Input : [1, 2, 4]
+Output : 125
+Explanation: 124 + 1 = 125 
+
+Input : [9, 9, 9]
+Output: 1000
+Explanation: 999 + 1 = 1000 
 '''
 
 
